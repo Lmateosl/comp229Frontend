58b00a626c254243f1bdeb1dc8a66f90af7244d1
estherdhimal <156553256+estherdhimal@users.noreply.github.com>
2024-11-30 18:06:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [news, setNews] = useState([]);
+  const [quesry, setQuery] = useState('');
+  const [category, setCategory] = useState('');
+  const[loading, setLoading] = useState(false);
 
   return (
-    <>
+    <> 
       News app
     </>
   )
