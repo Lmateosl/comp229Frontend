bf4469b6b2b3659516a91a3e2ffb129fa6076be7
estherdhimal <156553256+estherdhimal@users.noreply.github.com>
2024-11-30 19:26:58 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,21 @@ function App() {
   const [category, setCategory] = useState('');
   const[loading, setLoading] = useState(false);
 
+  const fetchNews = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('http://localhost:3000/news', {
+        params: { query, category }, 
+      });
+      setNews(response.data);
+    } catch (error) {
+      console.error ('Error fetching news:', error);
+      alert('Unable to fetch news. Please Try Again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <> 
       News app
