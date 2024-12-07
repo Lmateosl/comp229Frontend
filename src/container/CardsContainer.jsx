659bf0f93b0ce091d906e02af3a6b7ae79e9a6ce
Saurav <saurav.chaudhary3355@gmail.com>
2024-12-06 22:19:03 -0500

    Updated cardscontainer
    changed a styling a bit used Grid

--- src/container/CardsContainer.jsx
+++ src/container/CardsContainer.jsx
@@ -2,12 +2,16 @@ import '../App.css';
 import Grid from '@mui/material/Grid2';
 import CardNews from '../components/Card';
 
-export default function CardsContainer ({articles}) {
-    return(
+export default function CardsContainer({ articles }) {
+    return (
         <div className='articles_container'>
+            <Grid container spacing={2}> {}
                 {articles.map((article, i) => (
-                        <CardNews key={i} article={article}/>
+                    <Grid item xs={6} sm={6} md={4} lg={5} key={i}> {}
+                        <CardNews article={article} />
+                    </Grid>
                 ))}
+            </Grid>
         </div>
-    )
-} 
+    );
+}
