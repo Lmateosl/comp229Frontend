00aedf51a07785568d086988d4267683850a70e5
estherdhimal <156553256+estherdhimal@users.noreply.github.com>
2024-12-07 17:41:02 -0500

    overflowX

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,6 +3,8 @@ import Button from '@mui/material/Button';
 
 export default function Categories({handleClickCategorie}) {
     return(
+        <div style={{ display: 'flex', gap: '10px', justifyContent: 'flex-start', marginTop: '20px', overflowX: 'auto',
+            padding: '10px' }}>
         <Button variant="outlined" onClick={() => handleClickCategorie('world')}>World</Button>
         <Button variant="outlined" onClick={() => handleClickCategorie('sports')}>Sports</Button>
         <Button variant="outlined" onClick={() => handleClickCategorie('business')}>Business</Button>
@@ -11,7 +13,7 @@ export default function Categories({handleClickCategorie}) {
         <Button variant="outlined" onClick={() => handleClickCategorie('travel')}>Travel</Button>
         <Button variant="outlined" onClick={() => handleClickCategorie('politics')}>Politics</Button>
         <Button variant="outlined" onClick={() => handleClickCategorie('weather')}>Weather</Button>
-        <Button variant="outlined" onClick={() => handleClickCategorie('science')}>Science</Button>
+        <Button variant="outlined" onClick={() => handleClickCategorie('science')}>Science</Button>;
  </div>
     );
 }
