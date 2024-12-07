9be5ef5ed35d2ab1d11474d1d37d85136d8d7f1e
estherdhimal <156553256+estherdhimal@users.noreply.github.com>
2024-12-07 16:36:56 -0500

    Buttons added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,6 +3,15 @@ import Button from '@mui/material/Button';
 
 export default function Categories({handleClickCategorie}) {
     return(
+        <Button variant="outlined" onClick={() => handleClickCategorie('world')}>World</Button>
         <Button variant="outlined" onClick={() => handleClickCategorie('sports')}>Sports</Button>
-    )
+        <Button variant="outlined" onClick={() => handleClickCategorie('business')}>Business</Button>
+        <Button variant="outlined" onClick={() => handleClickCategorie('technology')}>Technology</Button>
+        <Button variant="outlined" onClick={() => handleClickCategorie('entertainment')}>Entertainment</Button>
+        <Button variant="outlined" onClick={() => handleClickCategorie('travel')}>Travel</Button>
+        <Button variant="outlined" onClick={() => handleClickCategorie('politics')}>Politics</Button>
+        <Button variant="outlined" onClick={() => handleClickCategorie('weather')}>Weather</Button>
+        <Button variant="outlined" onClick={() => handleClickCategorie('science')}>Science</Button>
+ </div>
+    );
 }
